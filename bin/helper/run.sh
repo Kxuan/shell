1a54d38fa88615001b364b622e4a2e1ee8463191
kXuan <kxuanobj@gmail.com>
2016-09-02 18:15:20 +0800

    run: run command with condition

---
+++ bin/helper/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+run() {
+    local opt arg
+    local pid
+    local mode
+    local signal=SIGINT
+    local sleeptime
+
+    local OPTIND=1
+    while getopts :ft:s: opt $@; do
+    case $opt in
+        f) mode=waitsuccess;;
+        t) mode=timed;sleeptime=$OPTARG;;
+        s) signal=$OPTARG;;
+    esac
+    shift $((OPTIND-1))
+    done
+
+    echo $?
+
+    case $mode in 
+        waitsuccess)
+            while ! $@;do :;done
+            ;;
+        timed)
+            $@ &
+            pid=$!
+            sleep $sleeptime
+            kill -$signal $pid
+            wait $pid
+            ;;
+    esac 
+}
--- bin/helper/waitsucc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-utilok() {
-    until $@;do :; done
-}
