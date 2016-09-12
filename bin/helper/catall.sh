67209e2fcf1da933960ad43feb52a7abbffe7957
kXuan <kxuanobj@gmail.com>
2016-09-12 18:22:48 +0800

    catall: add '-v'. print current directory.

--- bin/helper/catall.sh
+++ bin/helper/catall.sh
@@ -3,6 +3,7 @@ __cat_all_file_in_dir() {
     local reverse=$2
     local next_line=$3
     local add_lf=$4
+    local verbose=$5
 
     for f in *; do
         if [[ -f $f ]]; then
@@ -12,7 +13,13 @@ __cat_all_file_in_dir() {
             $add_lf && echo
         elif $reverse && [[ -d $f ]]; then
             pushd -- $f >/dev/null || continue
-            __cat_all_file_in_dir $prefix$f'/' $reverse $next_line $add_lf
+            if $verbose; then
+                echo "Enter Directory: $PWD" >&2
+                __cat_all_file_in_dir $prefix$f'/' $reverse $next_line $add_lf $verbose
+                echo "Leave Directory: $PWD" >&2
+            else
+                __cat_all_file_in_dir $prefix$f'/' $reverse $next_line $add_lf $verbose
+            fi
             popd >/dev/null
         fi
     done
@@ -25,14 +32,16 @@ catall() {
     local next_line=false
     # insert a \n after file content
     local add_lf=false
+    local verbose=false
     local dirs=(.)
 
     OPTIND=0
-    while getopts :rnl opt; do
+    while getopts rclv opt; do
     case $opt in
         r) reverse=true;;
         c) add_lf=true;;
         l) next_line=true;;
+        v) verbose=true;;
     esac
     done
     shift $((OPTIND-1))
@@ -43,7 +52,7 @@ catall() {
     
     for dir in ${dirs[@]}; do
         pushd -- $dir >/dev/null || continue
-        __cat_all_file_in_dir "" $reverse $next_line $add_lf
+        __cat_all_file_in_dir "" $reverse $next_line $add_lf $verbose
         popd >/dev/null
     done
 }
