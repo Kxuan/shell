40911e991c834c07ab11dd77a3574e130bff1598
kXuan <kxuanobj@gmail.com>
2017-01-12 09:56:00 +0800

    proxy: local address

--- bin/helper/proxy.sh
+++ bin/helper/proxy.sh
@@ -1,16 +1,16 @@
-
+_PROXY_LOCAL_ADDRESS=127.0.0.1:8123
 proxy_setup() {
-    export http_proxy=http://127.0.0.1:8123/
-    export https_proxy=http://127.0.0.1:8123/
+    export http_proxy=http://${_PROXY_LOCAL_ADDRESS}/
+    export https_proxy=http://${_PROXY_LOCAL_ADDRESS}/
 }
 proxy_start() {
-    ssh lufq@192.168.20.95 -NnCf -L8123:127.0.0.1:8123
+    ssh lufq@192.168.20.95 -NnCf -L*:8123:${_PROXY_LOCAL_ADDRESS}
     proxy_setup
 }
 proxy_stop() {
     local pid
-    pid=$(netstat -ntpl 2>/dev/null | grep '127.0.0.1:8123'| sed -e 's/^.*\s\([0-9][0-9]*\)\/ssh.*$/\1/g')
-    ps $pid
+    pid=$(ss -Hnp -4 state listening 'src '${_PROXY_LOCAL_ADDRESS}'' | grep -oP 'pid=\d+' | cut -d= -f2)
+    ps "$pid" || exit 1
     read -n1 -p "Kill the process? [Y/n]" ans
     if [[ $ans != "n" ]] && [[ $ans != "N" ]]; then
         echo "Killing $pid"
@@ -20,6 +20,6 @@ proxy_stop() {
     unset https_proxy
 }
 proxy_check() {
-    netstat -ntpl 2>/dev/null | grep -q '127.0.0.1:8123'
+    netstat -ntpl 2>/dev/null | grep -q '${_PROXY_LOCAL_ADDRESS}'
 }
 proxy_check && proxy_setup && echo "Proxy [ok]"
