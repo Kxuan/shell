535588b1d3ff247a452efd239cac3956d0a98650
kXuan <kxuanobj@gmail.com>
2017-01-18 12:16:31 +0800

    pkg: new action matcher

--- bin/helper/pkg.sh
+++ bin/helper/pkg.sh
@@ -14,27 +14,31 @@ __pkg_complete_binary() {
          IFS=$'\n'
      done < <(pkgfile -rv '/s?bin/'$1'\w*$') 
 }
+__pkg_action_match() {
+    bash -ic '. `which pkg`; _pkg_action_match "'$1'"'
+}
 __pkg_complete() {
     local cur=$2
     COMPREPLY=()
 
-    #Complete the action
     if ((COMP_CWORD == 1)); then
-        _arch_compgen \
-a add bi binary f file filename i ib if info inst install installbinary iu l list path r re remove s se search su sys systemupgrade test u ui un uninstall up update
-
+        _arch_compgen $(__pkg_action_match \*)
         return 0
     fi
 
-    local action=${COMP_WORDS[1]}
-    case $action in 
-        i|inst|install|a|add|s|se|search|l|list|if|info)
-            _pacman_pkg Slq;;
-        r|re|remove|un|uninstall)
-            _pacman_pkg Qqe;;
-        b|bin|binary)
-            __pkg_complete_binary $cur;;
-    esac 
+    local action=($(__pkg_action_match ${COMP_WORDS[1]}))
+    case ${#action[@]} in 
+        0) return ;;
+        1) case ${action[0]} in 
+            install|search|list|info)
+                _pacman_pkg Slq;;
+            remove|uninstall)
+                _pacman_pkg Qqe;;
+            bin*)
+                __pkg_complete_binary $cur;;
+           esac ;;
+        *) COMPREPLY=(${action[@]}) ;;
+    esac
     return 0
 }
 if [[ -n $BASH ]]; then
